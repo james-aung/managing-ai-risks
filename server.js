8e8e0108bea89fb84b660e4d8b4dba5d992d7c89
sandfront <therealaung@gmail.com>
2023-10-24 01:42:20 +0000

    latest production

--- server.js
+++ server.js
@@ -3,22 +3,22 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
-// Middleware to restrict access to specific users
-function requireSpecificUser(req, res, next) {
-  // Replace with the usernames you want to allow
-  const allowedUsernames = ['JamesAung', 'janbrauner', 'SoerenMind'];
-  const username = req.headers['x-replit-user-name'];
+// // Middleware to restrict access to specific users
+// function requireSpecificUser(req, res, next) {
+//   // Replace with the usernames you want to allow
+//   const allowedUsernames = ['JamesAung', 'janbrauner', 'SoerenMind'];
+//   const username = req.headers['x-replit-user-name'];
 
-  if (username && allowedUsernames.includes(username)) {
-    next();
-  } else {
-    res.status(403).send('Forbidden');
-  }
-}
+//   if (username && allowedUsernames.includes(username)) {
+//     next();
+//   } else {
+//     res.status(403).send('Forbidden');
+//   }
+// }
 
 
-// Comment out the following line to disable authentication and remember to turn off Replit Auth
-app.use(requireSpecificUser);
+// // Comment out the following line to disable authentication and remember to turn off Replit Auth
+// app.use(requireSpecificUser);
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
